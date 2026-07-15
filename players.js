f2752e7d0ab66bfc18012b1db1d8af6c3170a203
kusseleague <aritsingh.25@gmail.com>
2026-07-15 18:47:37 +0100

    players.js

--- players.js
+++ players.js
@@ -3,6 +3,8 @@
 const players = [
 
 {
+
+{
 name: "Virat Kohli",
 role: "Batsman",
 bat: 94,
@@ -19,147 +21,226 @@ stamina: 84
 },
 
 {
-name: "Babar Azam",
+name: "Shreyas Iyer",
 role: "Batsman",
-bat: 91,
+bat: 93,
 bowl: 10,
-stamina: 86
+stamina: 88
 },
 
 {
 name: "Steve Smith",
 role: "Batsman",
 bat: 92,
-bowl: 40,
-stamina: 91
-},
-
-{
-name: "Joe Root",
-role: "Batsman",
-bat: 90,
 bowl: 35,
-stamina: 90
-},
-
-{
-name: "Jasprit Bumrah",
-role: "Bowler",
-bat: 20,
-bowl: 96,
-stamina: 92
-},
-
-{
-name: "Mitchell Starc",
-role: "Bowler",
-bat: 25,
-bowl: 94,
-stamina: 88
-},
-
-{
-name: "Pat Cummins",
-role: "Bowler",
-bat: 35,
-bowl: 95,
-stamina: 93
-},
-
-{
-name: "Rashid Khan",
-role: "Bowler",
-bat: 40,
-bowl: 93,
-stamina: 91
-},
-
-{
-name: "Trent Boult",
-role: "Bowler",
-bat: 18,
-bowl: 92,
-stamina: 90
-},
-
-{
-name: "Ben Stokes",
-role: "All Rounder",
-bat: 92,
-bowl: 88,
-stamina: 94
-},
-
-{
-name: "Hardik Pandya",
-role: "All Rounder",
-bat: 88,
-bowl: 85,
-stamina: 88
-},
-
-{
-name: "Glenn Maxwell",
-role: "All Rounder",
-bat: 89,
-bowl: 82,
 stamina: 87
 },
 
 {
-name: "Ravindra Jadeja",
-role: "All Rounder",
-bat: 85,
-bowl: 92,
-stamina: 93
-},
-
-{
-name: "Shakib Al Hasan",
-role: "All Rounder",
+name: "Josh Inglis",
+role: "Wicketkeeper",
 bat: 90,
-bowl: 89,
-stamina: 92
+bowl: 40,
+stamina: 90
 },
 
 {
 name: "Kane Williamson",
 role: "Batsman",
-bat: 93,
+bat: 88,
 bowl: 25,
-stamina: 92
+stamina: 86
 },
 
 {
 name: "David Warner",
 role: "Batsman",
-bat: 89,
+bat: 94,
+bowl: 10,
+stamina: 86
+},
+
+{
+name: "Shubman Gill",
+role: "Batsman",
+bat: 92,
+bowl: 15,
+stamina: 88
+},
+
+{
+name: "Faf du Plessis",
+role: "Batsman",
+bat: 88,
+bowl: 10,
+stamina: 87
+},
+
+{
+name: "Aiden Markram",
+role: "Batsman",
+bat: 86,
+bowl: 55,
+stamina: 88
+},
+
+{
+name: "S Dhawan",
+role: "Batsman",
+bat: 86,
+bowl: 10,
+stamina: 85
+},
+
+{
+name: "Travis Head",
+role: "Batsman",
+bat: 90,
+bowl: 35,
+stamina: 87
+},
+
+{
+name: "D Conway",
+role: "Batsman",
+bat: 87,
+bowl: 30,
+stamina: 84
+},
+
+{
+name: "Harry Brook",
+role: "Batsman",
+bat: 86,
+bowl: 15,
+stamina: 85
+},
+
+{
+name: "Joe Root",
+role: "Batsman",
+bat: 84,
+bowl: 10,
+stamina: 82
+},
+
+{
+name: "W Saha",
+role: "Wicketkeeper",
+bat: 87,
+bowl: 10,
+stamina: 90
+},
+
+{
+name: "Jos Buttler",
+role: "Wicketkeeper",
+bat: 91,
+bowl: 10,
+stamina: 88
+},
+
+{
+name: "Quinton de Kock",
+role: "Wicketkeeper",
+bat: 86,
+bowl: 10,
+stamina: 84
+},
+
+{
+name: "Ruturaj Gaikwad",
+role: "Batsman",
+bat: 84,
+bowl: 10,
+stamina: 86
+},
+
+{
+name: "Yashasvi Jaiswal",
+role: "Batsman",
+bat: 88,
+bowl: 10,
+stamina: 87
+},
+
+{
+name: "Suryakumar Yadav",
+role: "Batsman",
+bat: 90,
+bowl: 20,
+stamina: 86
+},
+
+{
+name: "Ibrahim Zadran",
+role: "Batsman",
+bat: 82,
+bowl: 10,
+stamina: 84
+},
+
+{
+name: "Rahmanullah Gurbaz",
+role: "Wicketkeeper",
+bat: 82,
 bowl: 10,
 stamina: 83
 },
 
 {
-name: "Shaheen Afridi",
-role: "Bowler",
-bat: 20,
-bowl: 92,
-stamina: 87
+name: "Tom Latham",
+role: "Wicketkeeper",
+bat: 84,
+bowl: 10,
+stamina: 88
 },
 
 {
-name: "Mohammed Shami",
-role: "Bowler",
-bat: 28,
-bowl: 92,
-stamina: 90
+name: "Henry Nicholls",
+role: "Batsman",
+bat: 80,
+bowl: 10,
+stamina: 84
 },
 
 {
-name: "Andre Russell",
-role: "All Rounder",
-bat: 91,
-bowl: 86,
+name: "Temba Bavuma",
+role: "Batsman",
+bat: 78,
+bowl: 10,
+stamina: 82
+},
+
+{
+name: "Nicholas Pooran",
+role: "Wicketkeeper",
+bat: 86,
+bowl: 10,
+stamina: 84
+},
+
+{
+name: "Kusal Mendis",
+role: "Wicketkeeper",
+bat: 82,
+bowl: 10,
+stamina: 82
+},
+
+{
+name: "KL Rahul",
+role: "Wicketkeeper",
+bat: 90,
+bowl: 10,
+stamina: 89
+},
+
+{
+name: "Abhishek Sharma",
+role: "Batsman",
+bat: 87,
+bowl: 15,
 stamina: 85
-}
-
+},
 ];
