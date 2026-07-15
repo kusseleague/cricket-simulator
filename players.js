f4b8861e3025f0012eb027656e4d2cc3b8ca8278
kusseleague <aritsingh.25@gmail.com>
2026-07-15 18:14:26 +0100

    players.js

--- players.js
+++ players.js
@@ -17,11 +17,51 @@ stamina: 84
 },
 
 {
+name: "Babar Azam",
+role: "Batsman",
+bat: 91,
+bowl: 10,
+stamina: 86
+},
+
+{
+name: "Steve Smith",
+role: "Batsman",
+bat: 92,
+bowl: 40,
+stamina: 91
+},
+
+{
+name: "Joe Root",
+role: "Batsman",
+bat: 90,
+bowl: 35,
+stamina: 90
+},
+
+{
 name: "Jasprit Bumrah",
 role: "Bowler",
 bat: 20,
 bowl: 96,
 stamina: 92
+},
+
+{
+name: "Mitchell Starc",
+role: "Bowler",
+bat: 25,
+bowl: 94,
+stamina: 88
+},
+
+{
+name: "Pat Cummins",
+role: "Bowler",
+bat: 35,
+bowl: 95,
+stamina: 93
 },
 
 {
@@ -33,11 +73,91 @@ stamina: 91
 },
 
 {
+name: "Trent Boult",
+role: "Bowler",
+bat: 18,
+bowl: 92,
+stamina: 90
+},
+
+{
 name: "Ben Stokes",
 role: "All Rounder",
 bat: 92,
 bowl: 88,
 stamina: 94
+},
+
+{
+name: "Hardik Pandya",
+role: "All Rounder",
+bat: 88,
+bowl: 85,
+stamina: 88
+},
+
+{
+name: "Glenn Maxwell",
+role: "All Rounder",
+bat: 89,
+bowl: 82,
+stamina: 87
+},
+
+{
+name: "Ravindra Jadeja",
+role: "All Rounder",
+bat: 85,
+bowl: 92,
+stamina: 93
+},
+
+{
+name: "Shakib Al Hasan",
+role: "All Rounder",
+bat: 90,
+bowl: 89,
+stamina: 92
+},
+
+{
+name: "Kane Williamson",
+role: "Batsman",
+bat: 93,
+bowl: 25,
+stamina: 92
+},
+
+{
+name: "David Warner",
+role: "Batsman",
+bat: 89,
+bowl: 10,
+stamina: 83
+},
+
+{
+name: "Shaheen Afridi",
+role: "Bowler",
+bat: 20,
+bowl: 92,
+stamina: 87
+},
+
+{
+name: "Mohammed Shami",
+role: "Bowler",
+bat: 28,
+bowl: 92,
+stamina: 90
+},
+
+{
+name: "Andre Russell",
+role: "All Rounder",
+bat: 91,
+bowl: 86,
+stamina: 85
 }
 
 ];
