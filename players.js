4b6afaed79a2481d5b1d5115adc6c838cd1e7f2a
kusseleague <aritsingh.25@gmail.com>
2026-07-26 09:39:05 +0100

    Update players.js

--- players.js
+++ players.js
@@ -15,119 +15,63 @@ name: "Rohit Sharma",
 role: "Batsman",
 bat: 92,
 bowl: 15,
-stamina: 84
-},
-
-{
-name: "Shreyas Iyer",
-role: "Batsman",
-bat: 93,
-bowl: 10,
-stamina: 88
-},
-
-{
-name: "Steve Smith",
-role: "Batsman",
-bat: 92,
-bowl: 35,
-stamina: 87
-},
-
-{
-name: "Josh Inglis",
-role: "Wicketkeeper",
-bat: 90,
-bowl: 40,
-stamina: 90
-},
-
-{
-name: "Kane Williamson",
-role: "Batsman",
-bat: 88,
-bowl: 25,
-stamina: 86
-},
-
-{
-name: "David Warner",
-role: "Batsman",
-bat: 94,
-bowl: 10,
-stamina: 86
+stamina: 85
 },
 
 {
 name: "Shubman Gill",
 role: "Batsman",
-bat: 92,
+bat: 90,
 bowl: 15,
 stamina: 88
 },
 
 {
-name: "Faf du Plessis",
+name: "Shreyas Iyer",
 role: "Batsman",
+bat: 86,
+bowl: 10,
+stamina: 86
+},
+
+{
+name: "Suryakumar Yadav",
+role: "Batsman",
+bat: 91,
+bowl: 20,
+stamina: 87
+},
+
+{
+name: "Yashasvi Jaiswal",
+role: "Batsman",
+bat: 89,
+bowl: 10,
+stamina: 88
+},
+
+{
+name: "Ruturaj Gaikwad",
+role: "Batsman",
+bat: 86,
+bowl: 10,
+stamina: 86
+},
+
+{
+name: "Rishabh Pant",
+role: "Wicketkeeper",
 bat: 88,
 bowl: 10,
 stamina: 87
 },
 
 {
-name: "Aiden Markram",
-role: "Batsman",
-bat: 86,
-bowl: 55,
-stamina: 88
-},
-
-{
-name: "S Dhawan",
-role: "Batsman",
-bat: 86,
-bowl: 10,
-stamina: 85
-},
-
-{
-name: "Travis Head",
-role: "Batsman",
-bat: 90,
-bowl: 35,
-stamina: 87
-},
-
-{
-name: "D Conway",
-role: "Batsman",
-bat: 87,
-bowl: 30,
-stamina: 84
-},
-
-{
-name: "Harry Brook",
-role: "Batsman",
-bat: 86,
-bowl: 15,
-stamina: 85
-},
-
-{
-name: "Joe Root",
-role: "Batsman",
-bat: 84,
-bowl: 10,
-stamina: 82
-},
-
-{
-name: "W Saha",
+name: "KL Rahul",
 role: "Wicketkeeper",
-bat: 87,
+bat: 88,
 bowl: 10,
-stamina: 90
+stamina: 88
 },
 
 {
@@ -135,7 +79,87 @@ name: "Jos Buttler",
 role: "Wicketkeeper",
 bat: 91,
 bowl: 10,
+stamina: 87
+},
+
+{
+name: "Jonny Bairstow",
+role: "Wicketkeeper",
+bat: 84,
+bowl: 10,
+stamina: 82
+},
+
+{
+name: "Steve Smith",
+role: "Batsman",
+bat: 92,
+bowl: 35,
+stamina: 91
+},
+
+{
+name: "David Warner",
+role: "Batsman",
+bat: 88,
+bowl: 10,
+stamina: 84
+},
+
+{
+name: "Travis Head",
+role: "Batsman",
+bat: 91,
+bowl: 35,
 stamina: 88
+},
+
+{
+name: "Marnus Labuschagne",
+role: "Batsman",
+bat: 86,
+bowl: 25,
+stamina: 89
+},
+
+{
+name: "Kane Williamson",
+role: "Batsman",
+bat: 90,
+bowl: 25,
+stamina: 90
+},
+
+{
+name: "Joe Root",
+role: "Batsman",
+bat: 91,
+bowl: 40,
+stamina: 91
+},
+
+{
+name: "Harry Brook",
+role: "Batsman",
+bat: 87,
+bowl: 15,
+stamina: 86
+},
+
+{
+name: "Dawid Malan",
+role: "Batsman",
+bat: 84,
+bowl: 10,
+stamina: 82
+},
+
+{
+name: "Ben Duckett",
+role: "Batsman",
+bat: 84,
+bowl: 10,
+stamina: 85
 },
 
 {
@@ -143,11 +167,19 @@ name: "Quinton de Kock",
 role: "Wicketkeeper",
 bat: 86,
 bowl: 10,
-stamina: 84
+stamina: 85
 },
 
 {
-name: "Ruturaj Gaikwad",
+name: "Heinrich Klaasen",
+role: "Wicketkeeper",
+bat: 89,
+bowl: 10,
+stamina: 86
+},
+
+{
+name: "Rassie van der Dussen",
 role: "Batsman",
 bat: 84,
 bowl: 10,
@@ -155,47 +187,15 @@ stamina: 86
 },
 
 {
-name: "Yashasvi Jaiswal",
+name: "Aiden Markram",
 role: "Batsman",
-bat: 88,
-bowl: 10,
-stamina: 87
-},
-
-{
-name: "Suryakumar Yadav",
-role: "Batsman",
-bat: 90,
-bowl: 20,
-stamina: 86
-},
-
-{
-name: "Ibrahim Zadran",
-role: "Batsman",
-bat: 82,
-bowl: 10,
-stamina: 84
-},
-
-{
-name: "Rahmanullah Gurbaz",
-role: "Wicketkeeper",
-bat: 82,
-bowl: 10,
-stamina: 83
-},
-
-{
-name: "Tom Latham",
-role: "Wicketkeeper",
-bat: 84,
-bowl: 10,
+bat: 85,
+bowl: 55,
 stamina: 88
 },
 
 {
-name: "Henry Nicholls",
+name: "Temba Bavuma",
 role: "Batsman",
 bat: 80,
 bowl: 10,
@@ -203,42 +203,41 @@ stamina: 84
 },
 
 {
-name: "Temba Bavuma",
+name: "Devon Conway",
 role: "Batsman",
-bat: 78,
+bat: 86,
+bowl: 20,
+stamina: 86
+},
+
+{
+name: "Tom Latham",
+role: "Wicketkeeper",
+bat: 83,
 bowl: 10,
-stamina: 82
+stamina: 87
+},
+
+{
+name: "Glenn Phillips",
+role: "Batsman",
+bat: 84,
+bowl: 70,
+stamina: 90
 },
 
 {
 name: "Nicholas Pooran",
 role: "Wicketkeeper",
-bat: 86,
+bat: 87,
 bowl: 10,
 stamina: 84
 },
 
 {
-name: "Kusal Mendis",
+name: "Shai Hope",
 role: "Wicketkeeper",
-bat: 82,
+bat: 84,
 bowl: 10,
-stamina: 82
+stamina: 86
 },
-
-{
-name: "KL Rahul",
-role: "Wicketkeeper",
-bat: 90,
-bowl: 10,
-stamina: 89
-},
-
-{
-name: "Abhishek Sharma",
-role: "Batsman",
-bat: 87,
-bowl: 15,
-stamina: 85
-},
-];
