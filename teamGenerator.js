bd9962e81359bd8471527bdddc163f6012d7919a
kusseleague <aritsingh.25@gmail.com>
2026-09-23 22:30:01 +0100

    teamGenerator.js

--- teamGenerator.js
+++ teamGenerator.js
@@ -1144,7 +1144,24 @@ if(result === "W"){
     commentary =
         `💥 OUT! ${striker.name} dismissed by ${currentBowler.name}`;
 
+balls++;
 
+if(secondInnings){
+
+    secondInningsBalls++;
+
+}
+else{
+
+    firstInningsBalls++;
+
+}
+
+if(currentBowler && bowlerStats[currentBowler.name]){
+
+    bowlerStats[currentBowler.name].balls++;
+
+}
 
     // ===============================
     // ALL OUT
